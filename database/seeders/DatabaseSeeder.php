82ff82a6fbe6e1304b60aa98a7fd701e5eb6be4b
kamransparking <kamran.sparkingasia@gmail.com>
2026-08-17 17:03:53 +0500

    Test deployment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            LeadSeeder::class,
+            // LeadSeeder::class,
             AdminUserSeeder::class
         ]);
     }
